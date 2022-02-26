fd4597f9fa52025d6ea7c64ef826919d0903ff1d
GerganaRoeva <gergana.roeva@gmail.com>
2022-02-26 19:49:51 +0200

    basic navbar and style

--- web-app-token-generator/src/App.vue
+++ web-app-token-generator/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Nav />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from "./components/Nav.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Nav,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  margin: 0;
 }
 </style>
---
+++ web-app-token-generator/src/components/Nav.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex-container">
+    <button class="home">
+      <a href="/"> ERC-20 TOKEN GENERATOR</a>
+    </button>
+    <button class="dict">
+      <a href="/dictionary"> Dictionary</a>
+    </button>
+  </div>
+</template>
+
+
+<style scoped>
+
+div {
+  background-color: rgb(152, 250, 245);
+  margin: 0;
+  padding: 0;
+  height: 85px;
+}
+.flex-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+}
+button.home {
+  font-size: 20px;
+  background-color: transparent;
+  background-repeat: no-repeat;
+  border: none;
+  overflow: hidden;
+  outline: none;
+}
+
+button.dict {
+  border: none;
+  background-color: transparent;
+  float: right;
+  /* padding-right: 300px; */
+  overflow: hidden;
+  outline: none;
+}
+
+a {
+  text-decoration: none;
+  color: rgb(0, 0, 0);
+}
+</style>
