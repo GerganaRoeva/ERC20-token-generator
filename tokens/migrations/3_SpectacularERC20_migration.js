985591514d654c09f4f4558c06353817755f99e2
GerganaRoeva <gergana.roeva@gmail.com>
2022-03-23 11:36:40 +0200

    renamed supplycap

--- tokens/migrations/3_SpectacularERC20_migration.js
+++ tokens/migrations/3_SpectacularERC20_migration.js
@@ -3,8 +3,8 @@ const SpectacularERC20 = artifacts.require("SpectacularERC20");
 module.exports = function (deployer, name, symbol, supply, decimals) {
     name = "NAME";
     symbol = "NM";
-    supply = 20000;
+    supplyCap = 20000;
     decimals = 10;
     initalSupply = 1000;
-  deployer.deploy(SpectacularERC20, name, symbol, supply,initalSupply, decimals);
+  deployer.deploy(SpectacularERC20, name, symbol, supplyCap, initalSupply, decimals);
 };
