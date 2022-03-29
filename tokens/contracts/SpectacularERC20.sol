765552968103e9fdb29c5a86567a3f313208b68f
GerganaRoeva <gergana.roeva@gmail.com>
2022-03-29 15:00:38 +0300

    rm unnecessary funcs

--- tokens/contracts/SpectacularERC20.sol
+++ tokens/contracts/SpectacularERC20.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.0;
 
 import "./utils/MintBurnFuncs.sol";
 import "./utils/OwnableAccess.sol";
-// import "./BasicERC20.sol";
-
 
 contract SpectacularERC20 is MintBurnFuncs, OwnableAccess{
 
@@ -18,7 +16,6 @@ contract SpectacularERC20 is MintBurnFuncs, OwnableAccess{
         uint8 decimals_
     ) BasicERC20(name_, symbol_, initalSupply_, decimals_) OwnableAccess(){
         _capSupply = capSupply_;
-        _initialSupply = initalSupply_;
         _balances[msg.sender] = initalSupply_;
     }
 
@@ -31,87 +28,8 @@ contract SpectacularERC20 is MintBurnFuncs, OwnableAccess{
         return true;
     }
 
-    function allowance(address owner, address spender) public view virtual override returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
     function burn(address account, uint256 amount) public onlyOwner returns (bool){
         _burn(account, amount);
         return true;
     }
-
-    function transfer (
-        address to,
-        uint256 amount
-    ) public virtual override returns (bool) {
-        address owner =  msg.sender;
-        _transfer(owner, to, amount);
-        return true;
-    }
-
-    function transferFrom (
-        address from,
-        address to,
-        uint256 amount
-    ) public virtual override returns (bool) {
-        address spender =  msg.sender;
-        _spendAllowance(from, spender, amount);
-        _transfer(from, to, amount);
-        return true;
-    }
-
-    function approve(
-        address spender,
-        uint256 amount
-    ) public virtual override returns (bool) {
-        address owner =  msg.sender;
-        _approve(owner, spender, amount);
-        return true;
-    }
-
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal virtual override{
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-
-    function _transfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override{
-        require(from != address(0), "ERC20: transfer from the zero address");
-        require(to != address(0), "ERC20: transfer to the zero address");
-
-        uint256 fromBalance = _balances[from];
-        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
-        unchecked {
-            _balances[from] = fromBalance - amount;
-        }
-        _balances[to] += amount;
-
-        emit Transfer(from, to, amount);
-
-    }
-
-    function _spendAllowance(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal virtual override{
-        uint256 currentAllowance = allowance(owner, spender);
-        if (currentAllowance != type(uint256).max) {
-            require(currentAllowance >= amount, "ERC20: insufficient allowance");
-            unchecked {
-                _approve(owner, spender, currentAllowance - amount);
-            }
-        }
-    }
-
 }
