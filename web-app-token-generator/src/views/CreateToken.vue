e4b2cde562b81fca2d74b76c5e55091b60d06fc0
GerganaRoeva <gergana.roeva@gmail.com>
2022-03-29 15:06:37 +0300

    choose token componedt finished

--- web-app-token-generator/src/views/CreateToken.vue
+++ web-app-token-generator/src/views/CreateToken.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="container">
     <InstallMetaMask />
-    <ChooseToken @token="getType" />
-    <Form :typeOfToken="tokenType" />
+    <h2>Creating token</h2>
+    <section class="token-and-form">
+      <ChooseToken @token="getType" />
+      <Form :typeOfToken="tokenType" />
+    </section>
   </div>
 </template>
+
+<style scoped>
+h2 {
+  text-align: center;
+  font-size: 35px;
+}
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.token-and-form {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  flex-wrap: nowrap;
+}
+</style>
 
 <script>
 import InstallMetaMask from "../components/InstallMetaMask.vue";
 import ChooseToken from "../components/ChooseToken.vue";
 import Form from "../components/Form.vue";
-
-
 
 export default {
   components: {
@@ -28,45 +48,6 @@ export default {
     getType(type) {
       this.tokenType = type;
     },
-
   },
 };
 </script>
-
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-}
-select {
-  height: 18px;
-  margin: 4px;
-}
-.token-input {
-  display: flex;
-  flex-direction: row;
-  height: 18px;
-  align-items: baseline;
-  margin: 10px;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-}
-.inputs {
-  display: flex;
-  flex-direction: column;
-}
-.create-button {
-  background-color: aquamarine;
-  padding: 14px 25px;
-  text-align: center;
-  display: inline-block;
-  border-radius: 12px;
-}
-.create-button:hover {
-  box-shadow: 0px 15px 20px rgba(106, 255, 196, 0.507);
-}
-a {
-  text-decoration: none;
-  color: white;
-}
-</style>
