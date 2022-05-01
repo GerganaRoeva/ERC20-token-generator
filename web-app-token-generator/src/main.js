5c6b7af1e8768d8ceed016e78db88e39a41ceef6
GerganaRoeva <gergana.roeva@gmail.com>
2022-05-01 18:23:55 +0300

    added element ui framework

--- web-app-token-generator/src/main.js
+++ web-app-token-generator/src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router.js";
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import "element-plus/dist/index.css";
 
-createApp(App).use(router, ElementPlus).mount("#app");
-
+createApp(App).use(router).mount("#app");
