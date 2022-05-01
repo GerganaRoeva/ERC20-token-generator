16befe24a13357619e14a2a1b1fbf69f25951456
GerganaRoeva <gergana.roeva@gmail.com>
2022-05-01 18:20:47 +0300

    added date to migration

--- tokens/migrations/5_Election_migration.js
+++ tokens/migrations/5_Election_migration.js
@@ -3,8 +3,16 @@ const BasicERC20 = artifacts.require("BasicERC20");
 
 module.exports = function (deployer) {
   // for testing purposes
-  const start = 0;
-  const end = 0;
+  const start = new Date("July 27, 2020, 23:15:30")
+    .valueOf()
+    .toString()
+    .slice(0, 10);
+
+  const end = new Date("July 27, 2022, 23:15:30")
+    .valueOf()
+    .toString()
+    .slice(0, 10);
+
   const topic = "Should we something"
   deployer.deploy(Election, BasicERC20.address, start, end, topic);
 };
